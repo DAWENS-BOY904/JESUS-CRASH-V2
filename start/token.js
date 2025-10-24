602c06cb515a95b8691940a97c9aa2a6f12673e3
GOD-Da <dawiniefleury17@gmail.com>
2025-10-24 07:18:33 -0400

    Update token.js

--- start/token.js
+++ start/token.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: '8343781271:AAHIj8Xq2qL56lAM9LHkY0G224dt9rexV7M',  
+  BOT_TOKEN: '8347600713:AAH_oBECJatlKshAVt9O_gD-tZDKlTPPucE',  
   startupPassword: 'dawens'
 };
